e83ac93f9c31cc001f673fbb22407748cf9e778b
theBoneless25 <sebastianoocchipinti25@gmail.com>
2022-11-08 18:29:25 +0100

    08-11-22

--- 08-11-22/script.js
+++ 08-11-22/script.js
@@ -31,3 +31,24 @@ fetch("https://pokeapi.co/api/v2/pokemon?limit=150")
       )
     );
   });
+
+const colorCardPokemon = {
+  electric: "#FCF7DE",
+  water: "#DEF3FD",
+  ground: "#f4e7da",
+  rock: "#d5d5d4",
+  fairy: "#fceaff",
+  poison: "#98d7a5",
+  bug: "#f8d5a3",
+  dragon: "#97b3e6",
+  psychic: "#eaeda1",
+  flying: "#F5F5F5",
+  fighting: "#E6E0D4",
+  normal: "#F5F5F5",
+  fire: "#FDDFDF",
+  grass: "#DEFDE0",
+  ghost: "#705898",
+  ice: "#98d8d8",
+};
+
+///--HO PROVATO AD INSERIRE IL COLORE NEL BACKGROUND SOTTO CONSIGLIO DEL TUTOR MA NON SONO RIENTRATO NEL TEMPO
