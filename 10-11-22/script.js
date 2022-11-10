7501f60bc32c22cbc1d119b8a569ecac88eef249
theBoneless25 <sebastianoocchipinti25@gmail.com>
2022-11-10 17:13:59 +0100

    10-11-22

--- 10-11-22/script.js
+++ 10-11-22/script.js
@@ -1,13 +1,9 @@
 const btn = document.getElementById("btn");
-const advice = document.getElementById("advice");
+const advice = document.getElementById("advice"); // eseguo e dichiaro i vari id e la costante end point
 const urlAdv = "https://api.adviceslip.com/advice";
 
-/**
- * Get data from the end point
- *
- * @param {string} url
- */
 const getAdvice = (URL) => {
+  // eseguo il fetch e dichiaro la costante di get advice con URL come arg
   fetch(URL, { cache: "no-cache" })
     .then((res) => res.json())
     .then((res) => (advice.textContent = res.slip.advice))
@@ -16,10 +12,11 @@ const getAdvice = (URL) => {
 
 const onFirstLoad = () => {
   btn.addEventListener("click", () => {
+    //  funzione getadvice con all'interno parametro end point
     getAdvice(urlAdv);
   });
 
-  getAdvice(urlAdv);
+  getAdvice(urlAdv); // funzione
 };
 
-window.onload = onFirstLoad;
+window.onload = onFirstLoad; // al caricamento viene eseguite la funz. on first load
