fc4a68fae2c9963bbdaf891e90af47798a236337
theBoneless25 <sebastianoocchipinti25@gmail.com>
2022-11-14 17:30:26 +0100

    14-11-22

--- 14-11-22/main.js
+++ 14-11-22/main.js
@@ -1,9 +1,9 @@
-import { q, GET } from "./utils.js";
+import { q, GET } from "./utils.js"; /// IMPORTO LE UTILS
 
 const postId = q(".info_id");
 const postTitle = q(".title_post");
 const postBody = q(".body_post");
-const btnPrevious = q(".btn_prev");
+const btnPrevious = q(".btn_prev"); //// MI RICHIAMO LA STRUTTURA HTML
 const btnNext = q(".btn_next");
 
 const url = "https://jsonplaceholder.typicode.com/posts";
@@ -14,7 +14,7 @@ btnPrevious.addEventListener("click", (el) => {
   execute = el.target.textContent;
   GET(`${url}/${index}`).then((res) => {
     postId.textContent = ` ${res?.id}`;
-    postTitle.textContent = `${res?.title}`;
+    postTitle.textContent = `${res?.title}`; /// CREO GLI EVENTI DEI BOTTONI
     postBody.textContent = `${res?.body}`;
     getPost(execute);
     previousBtn(index);
@@ -38,7 +38,7 @@ const getPost = (execute) => {
     execute = 1;
   } else {
     execute = execute.toLowerCase();
-  }
+  } // COSTANTE CHE SI INCREMENTA O DECREMENTA
   switch (execute) {
     case "prev":
       index--;
@@ -51,7 +51,7 @@ const getPost = (execute) => {
 const nextBtn = (i) => {
   if (i >= 10) btnNext.disabled = true;
   if (i === 1) btnPrevious.disabled = true;
-  else btnPrevious.disabled = false;
+  else btnPrevious.disabled = false; // COSTANTE CHE DISABILITA E ABILITA IL BOTTONE AL RAGG DEL LIMITE
 };
 
 const previousBtn = (i) => {
