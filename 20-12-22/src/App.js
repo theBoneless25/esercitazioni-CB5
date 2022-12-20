41e0d9c1de7ad5b4194b86318034933a72be0429
theBoneless25 <sebastianoocchipinti25@gmail.com>
2022-12-20 16:24:45 +0100

    20-12-22

--- 20-12-22/src/App.js
+++ 20-12-22/src/App.js
@@ -2,8 +2,10 @@ import "./App.css";
 import { Button } from "./components/Button";
 
 function App() {
+  const text = "CIAO BELLI!!";
   return (
     <div className="App">
+      <h1>{text}</h1>
       <Button />
     </div>
   );
